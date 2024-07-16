389b3d4bf9d3b69647ea5e5d11a06fbe0407bd7f
Haemoffy <fuwansuu@gmail.com>
2024-07-16 18:05:22 +0800

    finished building history feature

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const operationKeys = document.querySelectorAll('[data-operations]')
 const resetKey = document.querySelector('[data-reset]')
 const equalsKey = document.querySelector('[data-equals]')
 const deleteKey = document.querySelector('[data-delete]')
-const historyPrevCalc = document.querySelectorAll('.prev-calculation')
+const calculations = document.querySelector('.calculations')
 let hasResult = false
-const calculations = []
 
 for (const numberKey of numberKeys) {
     numberKey.addEventListener('click', function(e) {
@@ -68,14 +67,17 @@ function addHistory(mathExpr, result) {
     sessionStorage.setItem('mathExpr', mathExpr)
     sessionStorage.setItem('result', result)
 
-    calculations.push({mathExpr: sessionStorage.getItem('mathExpr'), result: sessionStorage.getItem('result')})
-
-    console.log(calculations)
+    displayHistory(sessionStorage.getItem('mathExpr'), sessionStorage.getItem('result'))
 }
 
-function displayHistory() {
-    for (let i; i < calculations.length; i++) {
-        historyPrevCalc[i].querySelector('.expressions').textContent = calculations[i].mathExpr
-        historyPrevCalc[i].querySelector('.result').textContent = calculations[i].result
-    }
+function displayHistory(mathExpr, result) {
+    const prevCalc = document.createElement('div')
+    prevCalc.classList.add('prev-calculation')
+
+    prevCalc.innerHTML = `
+        <small class="expression">${mathExpr}&nbsp;=</small>
+        <div class="result">${result}</div>
+    `
+
+    calculations.prepend(prevCalc)
 }
